0bc75dbbea94dabc31ae40d96dd4f82250215420
ruslan0515015 <ruslaniosdev@gmail.com>
2023-10-05 19:24:44 +0400

    Created new project, deleted main storyboard file, added custom colors to UIColor using extension

---
+++ Helpers/UIColor+ Extension.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+import UIKit
+
+extension UIColor {
+    static var YPBackground: UIColor { UIColor(named:"YPBackground") ?? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1) }
+    static var YPBlack: UIColor { UIColor(named:"YPBlack") ?? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) }
+    static var YPBlue: UIColor { UIColor(named:"YPBlue") ?? #colorLiteral(red: 0.2695594132, green: 0.5159891248, blue: 1, alpha: 1) }
+    static var YPColorSelection1: UIColor { UIColor(named:"YPColorSelection1") ?? #colorLiteral(red: 1, green: 0.2304438651, blue: 0, alpha: 1) }
+    static var YPColorSelection2: UIColor { UIColor(named:"YPColorSelection2") ?? #colorLiteral(red: 1, green: 0.4682334661, blue: 0.1666698456, alpha: 1)}
+    static var YPColorSelection3: UIColor { UIColor(named:"YPColorSelection3") ?? #colorLiteral(red: 0.2105123997, green: 0.4191511273, blue: 1, alpha: 1)}
+    static var YPColorSelection4: UIColor { UIColor(named:"YPColorSelection4") ?? #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1) }
+    static var YPColorSelection5: UIColor { UIColor(named:"YPColorSelection5") ?? #colorLiteral(red: 0, green: 0.8079406619, blue: 0.2283389866, alpha: 1) }
+    static var YPColorSelection6: UIColor { UIColor(named:"YPColorSelection6") ?? #colorLiteral(red: 0.9742527604, green: 0.2188877463, blue: 0.6819146276, alpha: 1) }
+    static var YPColorSelection7: UIColor { UIColor(named:"YPColorSelection7") ?? #colorLiteral(red: 0.914505899, green: 0.657556653, blue: 0.81388551, alpha: 1) }
+    static var YPColorSelection8: UIColor { UIColor(named:"YPColorSelection8") ?? #colorLiteral(red: 0.3910667598, green: 0.5295843482, blue: 1, alpha: 1) }
+    static var YPColorSelection9: UIColor { UIColor(named:"YPColorSelection9") ?? #colorLiteral(red: 0.5942834616, green: 0.8659849167, blue: 0.5269940495, alpha: 1) }
+    static var YPColorSelection10: UIColor { UIColor(named:"YPColorSelection10") ?? #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1) }
+    static var YPColorSelection11: UIColor { UIColor(named:"YPColorSelection11") ?? #colorLiteral(red: 0.9680756927, green: 0.2906937897, blue: 0.1904996037, alpha: 1) }
+    static var YPColorSelection12: UIColor { UIColor(named:"YPColorSelection12") ?? #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1) }
+    static var YPColorSelection13: UIColor { UIColor(named:"YPColorSelection13") ?? #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1) }
+    static var YPColorSelection14: UIColor { UIColor(named:"YPColorSelection14") ?? #colorLiteral(red: 0.710957408, green: 0.7598259449, blue: 0.9068077207, alpha: 1) }
+    static var YPColorSelection15: UIColor { UIColor(named:"YPColorSelection15") ?? #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1) }
+    static var YPColorSelection16: UIColor { UIColor(named:"YPColorSelection16") ?? #colorLiteral(red: 0.841674149, green: 0.502810955, blue: 0.9175029993, alpha: 1) }
+    static var YPColorSelection17: UIColor { UIColor(named:"YPColorSelection17") ?? #colorLiteral(red: 0.6121326089, green: 0.4863414764, blue: 0.7473065257, alpha: 1) }
+    static var YPColorSelection18: UIColor { UIColor(named:"YPColorSelection18") ?? #colorLiteral(red: 0, green: 0.9326902628, blue: 0, alpha: 1) }
+    static var YPGray: UIColor { UIColor(named:"YPGray") ?? #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1) }
+    static var YPLightGray: UIColor { UIColor(named:"YPLightGray") ?? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1) }
+    static var YPRed: UIColor { UIColor(named:"YPRed") ?? #colorLiteral(red: 1, green: 0.3748272061, blue: 0.25904724, alpha: 1) }
+    static var YPWhite: UIColor { UIColor(named:"YPWhite") ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) }
+}
