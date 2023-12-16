5e0bd04ff15733f69659f4f9758365449577aca5
ruslan0515015 <ruslaniosdev@gmail.com>
2023-12-16 21:44:04 +0300

    Configured 2 page screens for UIPageViewController

--- Tracker/Onboarding/OnboardingViewController.swift
+++ Tracker/Onboarding/OnboardingViewController.swift
@@ -12,18 +12,42 @@ class OnboardingViewController: UIPageViewController {
         return [firstPage, secondPage]
     }()
     
+    private lazy var wowButton: UIButton = {
+        let button  = UIButton()
+        button.setTitle("Вот это технологии!", for: .normal)
+        button.backgroundColor = .YPBlack
+        button.tintColor = .YPWhite
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 16
+        button.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(button)
+        
+        return button
+    }()
+    
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        
+        return pageControl
+    }()
+    
     // MARK: - LifeCycle:
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        if let first = pages.first {
+            setViewControllers([first], direction: .forward, animated: true)
+        }
     }
     
-    // MARK: - Methods
-    
-    
     // MARK: - Private Methods:
-    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            wowButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            wowButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            wowButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -84)
+        ])
+    }
     
     
     
--- Tracker/Onboarding/Pages/FirstPageViewController.swift
+++ Tracker/Onboarding/Pages/FirstPageViewController.swift
@@ -2,13 +2,36 @@ import UIKit
 
 class FirstPageViewController: UIViewController {
     // MARK: - Private Properties:
-    private let image = UIImage(named: "firstScreen")
     private lazy var imageView: UIImageView = {
+        let image = UIImage(named: "firstScreen")
         let imageView = UIImageView(image: image)
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(imageView)
         return imageView
+    }()
+    
+    private lazy var praktikumLogoView: UIImageView = {
+        let logo = UIImage(named: "praktikumLogo")
+        let imageView = UIImageView(image: logo)
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(imageView)
+        
+        return imageView
+    }()
+    
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Отслеживайте только \nто, что хотите"
+        label.numberOfLines = 2
+        label.font = .boldSystemFont(ofSize: 32)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .YPWhite
+        self.view.addSubview(label)
+        
+        return label
     }()
     
     // MARK: - LifeCycle:
@@ -24,7 +47,17 @@ class FirstPageViewController: UIViewController {
             imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             imageView.topAnchor.constraint(equalTo: view.topAnchor),
-            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            praktikumLogoView.widthAnchor.constraint(equalToConstant: 94),
+            praktikumLogoView.heightAnchor.constraint(equalToConstant: 94),
+            praktikumLogoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            praktikumLogoView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            
+            textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textLabel.topAnchor.constraint(equalTo: praktikumLogoView.bottomAnchor, constant: 10)
+            
         ])
     }
     
