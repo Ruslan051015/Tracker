3dd21cebbc42e8b22acb23332b008f36bb8cb977
ruslan0515015 <ruslaniosdev@gmail.com>
2023-12-17 17:13:27 +0300

    Added UIPageViewController with two pagesViewController, next step to add pageControl

--- Tracker/Onboarding/OnboardingViewController.swift
+++ Tracker/Onboarding/OnboardingViewController.swift
@@ -13,7 +13,7 @@ class OnboardingViewController: UIPageViewController {
     }()
     
     private lazy var wowButton: UIButton = {
-        let button  = UIButton()
+        let button  = UIButton(type: .system)
         button.setTitle("Вот это технологии!", for: .normal)
         button.backgroundColor = .YPBlack
         button.tintColor = .YPWhite
@@ -35,9 +35,21 @@ class OnboardingViewController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        dataSource = self
+        setUpConstraints()
+        
         if let first = pages.first {
             setViewControllers([first], direction: .forward, animated: true)
         }
+    }
+    
+    // MARK: - Methods:
+    override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Private Methods:
@@ -45,7 +57,8 @@ class OnboardingViewController: UIPageViewController {
         NSLayoutConstraint.activate([
             wowButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             wowButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            wowButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -84)
+            wowButton.heightAnchor.constraint(equalToConstant: 60),
+            wowButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
         ])
     }
     
@@ -53,3 +66,32 @@ class OnboardingViewController: UIPageViewController {
     
 }
 // MARK: - Extensions:
+extension OnboardingViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else  {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        guard nextIndex < pages.count else {
+            return nil
+        }
+        return pages[nextIndex]
+    }
+    
+    
+}
